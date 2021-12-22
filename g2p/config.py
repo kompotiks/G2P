b3ca51ad4596279b2606e205597bda43f6dfbb14
kompotiks <lifami40@gmail.com>
2021-12-22 21:56:45 +0300

    Add hub_dir

--- g2p/config.py
+++ g2p/config.py
@@ -22,7 +22,8 @@ class ModelConfig(object):
             with open(DataConfig.phonemes_path) as f:
                 phonemes_size = len(json.load(f))
         except:
-            pass
+            graphemes_size = ''
+            phonemes_size = ''
         hidden_size = 128
 
 
