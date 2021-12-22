2982c7e6f6620e539ddceb1fa92a53806b6f9c77
kompotiks <lifami40@gmail.com>
2021-12-22 22:04:36 +0300

    Add hub_dir

--- hubconf.py
+++ hubconf.py
@@ -10,7 +10,7 @@ def g2p_ru():
         graphemes_size=34,
         hidden_size=128,
         phonemes_size=49,
-        encoder_model_path=f'{hub_dir}kompotiks_G2P_master/models/ru/encoder_e100.pth',
+        encoder_model_path=f'{hub_dir}/kompotiks_G2P_master/models/ru/encoder_e100.pth',
         decoder_model_path=f'{hub_dir}/kompotiks_G2P_master/models/ru/decoder_e100.pth',
     )
     return g2p
